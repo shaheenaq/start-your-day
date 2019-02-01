368376fc955bf254c4fa82c5ee4c27319257169c
Shaheena Qureshi <shaheenaq@gmail.com>
2019-02-01 17:37:02 -0500

    deletes function in app js

--- app.js
+++ app.js
@@ -28,42 +28,6 @@ let index = Math.floor(Math.random() * quotes.length);
 document.querySelector('#quote').innerHTML = `&#8220 ${quotes[index].content} &#8221 <br> ${quotes[index].author}`;
 
 
-// let lat;
-// let long;
-// let city;
-// let country;
-// let locData;
-// const urlLocate = "https://geoip-db.com/json/";
-
-// fetch(urlLocate).then(response => {
-// 	console.log(response);
-// 	response.json().then(data => {
-// 		console.log(data);
-// 		lat = data.latitude;
-// 		long = data.longitude;
-// 		city = data.city;
-// 		country = data.country_name;
-// 		let para1 = document.createElement('p');
-// 		para1.innerHTML = `Today's weather in ${city},${country}`;
-// 		document.getElementById('weather').appendChild(para1);
-
-// 		let weatherData;
-// 		const urlW = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=imperial&appid=e609adc51cd1d7c976b4c68b8e4e1962`;
-// 		fetch(urlW)
-// 			.then(response => response.json())
-// 			.then(data2 => {
-// 				console.log(data2);
-// 				weatherData = data2;
-// 				let para2 = document.createElement('p');
-// 				para2.innerHTML = `${weatherData.main.temp}&#8457 : ${weatherData.weather[0].description}`;
-// 				document.getElementById('weather').appendChild(para2);
-// 				let image1 = document.createElement('img');
-// 				image1.setAttribute('src','http://openweathermap.org/img/w/'+weatherData.weather[0].icon+'.png');
-// 				document.getElementById('weather').appendChild(image1);
-// 		});
-// 	});
-// });
-
 const handleClick = (e) => {
 
 	let userZipcode = document.getElementById('zipcode').value;
